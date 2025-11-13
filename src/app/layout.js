826b948bd9744587500d1693f32f334d4508d36b
Taimoor Gill <taimoorgill311@gmail.com>
2025-11-13 17:00:19 +0500

    Fav Icon Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,6 +29,9 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/laancer.ico" sizes="any" />
+      </head>
       <body className="bg-light">
         <SessionProvider>
           <BootstrapClient />
